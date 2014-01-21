91261251fdcddcab571a6722721cebdf438d6877
Karikoski Antti J <anttkari@da1-cs-bk107-39.helsinki.fi>
2014-01-21 13:26:28 +0200

    done first 5

--- app/models/beer.rb
+++ app/models/beer.rb
@@ -1,3 +1,9 @@
 class Beer < ActiveRecord::Base
   belongs_to :brewery
+  has_many :ratings
+
+  def average_rating
+    ratings.inject(0) {|result,rating| result + rating.score}/ratings.count
+    #ratings.average(:score).to_i
+  end
 end
